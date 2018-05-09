d0a28c711129bc7f4a562d576902254b82e81c55
Greg Karékinian <greg@karekinian.com>
2018-05-09 15:21:51 +0200

    Remove useless comments and remove duplicate section in the example configs

--- config.yml.example.swift
+++ config.yml.example.swift
@@ -1,9 +1,7 @@
 development: &defaults
   maintenance: false
-  # uncomment this section
   swift: &swift_defaults
     host: "https://swift.example.com"
-  # Redis is needed for the swift backend
   redis:
     host: localhost
     port: 6379
@@ -12,9 +10,6 @@ test:
   <<: *defaults
   swift:
     host: "https://swift.example.com"
-  redis:
-    host: localhost
-    port: 6379
 
 staging:
   <<: *defaults
