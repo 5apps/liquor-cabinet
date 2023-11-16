96417e3884c6bf275ce353a3005be44c86ac662c
Sebastian Kippe <sebastian@kip.pe>
2023-11-16 11:27:15 +0100

    Use development env and Webrick in dev run script

--- run-dev.sh
+++ run-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RACK_ENV=staging \
+RACK_ENV=development \
 REDIS_HOST=localhost \
 REDIS_PORT=6379 \
 REDIS_DB=1 \
@@ -8,4 +8,4 @@ S3_ENDPOINT='http://localhost:9000' \
 S3_ACCESS_KEY='dev-key' \
 S3_SECRET_KEY='123456789' \
 S3_BUCKET=remotestorage \
-bundle exec rainbows --listen localhost:4567
+bundle exec rackup -p 4567
